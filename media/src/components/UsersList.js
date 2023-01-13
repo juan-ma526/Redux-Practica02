05bcfd4ed0bd1dde6d3bebc2713c8c858913ae22
juan-ma526 <jmperez675@gmail.com>
2023-01-13 16:09:45 -0300

    utilizamos metodo asyn thunk

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -1,5 +1,71 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers, addUser } from "../store";
+import Skeleton from "./Skeleton";
+import Button from "./Button";
+
 const UsersList = () => {
-  return "User LIst";
+  const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+  const [loadingUsersError, setLoadingUsersError] = useState(null);
+  const [isCreatingUser, setIsCreatingUser] = useState(false);
+  const [creatingUserError, setCreatingUserError] = useState(null);
+  const dispatch = useDispatch();
+
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    setIsLoadingUsers(true);
+    dispatch(fetchUsers())
+      .unwrap()
+      .catch((err) => {
+        setLoadingUsersError(err);
+      })
+      .finally(() => {
+        setIsLoadingUsers(false);
+      });
+  }, [dispatch]);
+
+  const handleUserAdd = () => {
+    setIsCreatingUser(true);
+    dispatch(addUser())
+      .unwrap()
+      .catch((err) => setCreatingUserError(err))
+      .finally(() => setIsCreatingUser(false));
+  };
+
+  if (isLoadingUsers) {
+    return <Skeleton times={1} className="h-10 w-full" />;
+  }
+  if (loadingUsersError) {
+    return <div>Error fetching data...</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        {isCreatingUser ? (
+          "Se esta creando...."
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
+        {creatingUserError && "error creando usuario"}
+      </div>
+      {renderedUsers}
+    </div>
+  );
 };
 
 export default UsersList;
